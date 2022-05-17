3b9bbf8f561870cb44eb714e37c982412f48228f
luo <1127959736@qq.com>
2022-05-17 14:53:44 +0800

    implement the native method sun/misc/VM.initialize

---
+++ ch09/native/sun/misc/VM.go
@@ -0,0 +1,30 @@
+package misc
+
+import (
+	"jvmgo/ch09/instructions/base"
+	"jvmgo/ch09/native"
+	"jvmgo/ch09/rtda"
+	"jvmgo/ch09/rtda/heap"
+)
+
+func init() {
+	native.Register("sun/misc/VM", "initialize", "()V", initialize)
+}
+
+// private static native void initialize();
+// ()V
+func initialize(frame *rtda.Frame) { // hack: just make VM.savedProps nonempty
+	vmClass := frame.Method().Class()
+	savedProps := vmClass.GetRefVar("savedProps", "Ljava/util/Properties;")
+	key := heap.JString(vmClass.Loader(), "foo")
+	val := heap.JString(vmClass.Loader(), "bar")
+
+	frame.OperandStack().PushRef(savedProps)
+	frame.OperandStack().PushRef(key)
+	frame.OperandStack().PushRef(val)
+
+	propsClass := vmClass.Loader().LoadClass("java/util/Properties")
+	setPropMethod := propsClass.GetInstanceMethod("setProperty",
+		"(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/Object;")
+	base.InvokeMethod(frame, setPropMethod)
+}
