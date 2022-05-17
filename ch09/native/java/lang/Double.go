ce4ad8e167019dd54a5f3e2138772504c82cec7f
luo <1127959736@qq.com>
2022-05-17 12:49:03 +0800

    implement the native method java/lang/Float.doubleToRawIntBits and java/lang/Float.intBitsToDouble

---
+++ ch09/native/java/lang/Double.go
@@ -0,0 +1,30 @@
+package lang
+
+import (
+	"jvmgo/ch09/native"
+	"jvmgo/ch09/rtda"
+	"math"
+)
+
+const jlDouble = "java/lang/Double"
+
+func init() {
+	native.Register(jlDouble, "doubleToRawLongBits", "(D)J", doubleToRawLongBits)
+	native.Register(jlDouble, "longBitsToDouble", "(J)D", longBitsToDouble)
+}
+
+//public static native long doubleToRawLongBits(double value)
+// (D)]
+func doubleToRawLongBits(frame *rtda.Frame) {
+	value := frame.LocalVars().GetDouble(0)
+	bits := math.Float64bits(value)
+	frame.OperandStack().PushLong(int64(bits))
+}
+
+//public static native double longBitsToDouble(long bits)
+// (J)D
+func longBitsToDouble(frame *rtda.Frame) {
+	bits := frame.LocalVars().GetLong(0)
+	value := math.Float64frombits(uint64(bits))
+	frame.OperandStack().PushDouble(value)
+}
