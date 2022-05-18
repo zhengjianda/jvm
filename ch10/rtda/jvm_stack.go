f9bc076949ca1233df2ffd92c1479d2b347d07f4
luo <1127959736@qq.com>
2022-05-18 19:27:55 +0800

    add the method clear() to clear the stack and the getFrames() to get the frames of the stack

--- ch10/rtda/jvm_stack.go
+++ ch10/rtda/jvm_stack.go
@@ -48,3 +48,17 @@ func (self *Stack) top() *Frame {
 func (self *Stack) isEmpty() bool {
 	return self._top == nil
 }
+
+func (self *Stack) clear() {
+	for !self.isEmpty() {
+		self.pop()
+	}
+}
+
+func (self *Stack) getFrames() []*Frame {
+	frames := make([]*Frame, 0, self.size)
+	for frame := self._top; frame != nil; frame = frame.lower {
+		frames = append(frames, frame)
+	}
+	return frames
+}
