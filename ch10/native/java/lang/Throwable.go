d05e49b219c5b266f1afae03427c95e62d75f0fd
luo <1127959736@qq.com>
2022-05-18 19:32:36 +0800

    implements the native method Throwable fillInStackTrace(int dummy);

---
+++ ch10/native/java/lang/Throwable.go
@@ -0,0 +1,62 @@
+package lang
+
+import (
+	"fmt"
+	"jvmgo/ch10/native"
+	"jvmgo/ch10/rtda"
+	"jvmgo/ch10/rtda/heap"
+)
+
+type StackTraceElement struct {
+	fileName   string //给出类所在的文件名
+	className  string //给出声明方法的类名
+	methodName string //给出方法名
+	lineNumber int    //给出帧正在执行哪行代码
+}
+
+func init() {
+	native.Register("java/lang/Throwable", "fillInStackTrace", "(I)Ljava/lang/Throwable;", fillInStackTrace)
+}
+
+//private native Throwable fillInStackTrace(int dummy);
+func fillInStackTrace(frame *rtda.Frame) {
+	this := frame.LocalVars().GetThis()
+	frame.OperandStack().PushRef(this)
+	stes := createStackTraceElements(this, frame.Thread())
+	this.SetExtra(stes)
+}
+
+func createStackTraceElements(tObj *heap.Object, thread *rtda.Thread) []*StackTraceElement {
+	skip := distanceToObject(tObj.Class()) + 2 //掉过fillInStackTrace(int)和fillInStackTrace()
+	frames := thread.GetFrames()[skip:]
+	stes := make([]*StackTraceElement, len(frames))
+	for i, frame := range frames {
+		stes[i] = createStackTraceElement(frame)
+	}
+	return stes
+}
+
+//计算需要跳过多少正在执行异常类的构造函数的帧
+func distanceToObject(class *heap.Class) int {
+	distance := 0
+	for c := class.SuperClass(); c != nil; c = c.SuperClass() {
+		distance++
+	}
+	return distance
+}
+
+func createStackTraceElement(frame *rtda.Frame) *StackTraceElement {
+	method := frame.Method()
+	class := method.Class()
+	return &StackTraceElement{
+		fileName:   class.SourceFile(),
+		className:  class.JavaName(),
+		methodName: method.Name(),
+		lineNumber: method.GetLineNumber(frame.NextPC() - 1),
+	}
+}
+
+func (self *StackTraceElement) String() string {
+	return fmt.Sprintf("%s.%s(%s:%d)",
+		self.className, self.methodName, self.fileName, self.lineNumber)
+}
