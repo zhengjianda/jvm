07ed5e6cc924ac099fba94958c116885666d54eb
luo <1127959736@qq.com>
2022-06-18 19:42:48 +0800

    增加了一些代码注释

--- ch07/instructions/base/class_init_login.go
+++ ch07/instructions/base/class_init_login.go
@@ -10,6 +10,8 @@ func InitClass(thread *rtda.Thread, class *heap.Class) {
 	class.StartInit()
 	scheduleClinit(thread, class)
 	initSuperClass(thread, class)
+
+	//顺序细节 先初始化自己，此时只是把自己的初始化方法帧push进java虚拟机栈，然后再把父类初始化方法帧push进虚拟机栈，这样可以保证先初始化父类(因为父类的初始化帧在上，会先被JAVA解释器取出)，再初始化子类。
 }
 
 func scheduleClinit(thread *rtda.Thread, class *heap.Class) {
